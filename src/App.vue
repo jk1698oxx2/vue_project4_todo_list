cd2a804b08a84ef24dd5565826d2269d97a86480
Bolin <jk1698oxx2@outlook.com>
2026-05-25 11:37:06 +0800

    feat: add components for main function

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import TodoApp from './components/TodoApp.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <TodoApp />
 </template>
 
 <style scoped>
-@import './assets/main.css';
+  @import './assets/main.css';
 </style>
